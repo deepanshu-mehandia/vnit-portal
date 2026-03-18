c0a468ad6836ace940e54c5d73a16752f4d3e8c6
Deepanshu Mehandia <deepanshu2109mehandia@gmail.com>
2026-03-18 12:54:37 +0530

    Update charts.tsx

--- vnit-portal-frontend/components/charts.tsx
+++ vnit-portal-frontend/components/charts.tsx
@@ -1,45 +1,14 @@
 "use client"
 
-import { Line } from "react-chartjs-2"
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement
-} from "chart.js"
+import { BarChart, Bar, XAxis, YAxis } from "recharts"
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement
-)
+export default function Chart({data}:any){
 
-export default function Charts(){
-
-  const data = {
-    labels:["Sem1","Sem2","Sem3","Sem4"],
-    datasets:[
-      {
-        label:"CGPA Trend",
-        data:[7.5,7.8,8.0,8.2],
-        borderColor:"blue"
-      }
-    ]
-  }
-
-  return(
-
-    <div className="bg-white p-6 shadow rounded">
-
-      <h3 className="text-lg font-bold mb-4">
-        CGPA Trend
-      </h3>
-
-      <Line data={data} />
-
-    </div>
-
+  return (
+    <BarChart width={500} height={300} data={data}>
+      <XAxis dataKey="course" />
+      <YAxis />
+      <Bar dataKey="count" />
+    </BarChart>
   )
 }
