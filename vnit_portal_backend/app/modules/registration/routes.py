767bb1637fe7dcd9adb4588e6cf54bd58ac3d3bd
Deepanshu Mehandia <deepanshu2109mehandia@gmail.com>
2026-03-18 12:18:42 +0530

    Update routes.py

--- vnit_portal_backend/app/modules/registration/routes.py
+++ vnit_portal_backend/app/modules/registration/routes.py
@@ -5,32 +5,59 @@ from app.core.dependencies import require_role
 router = APIRouter(prefix="/registration")
 
 @router.post("/registration/add")
-def add_course(..., user=Depends(require_role("student"))):
-    return {"msg": "only students allowed"}
-
 def add_course(student_id: int, offering_id: int):
 
     conn = get_connection()
     cur = conn.cursor()
+
     try:
-        # check already registered
+
+        # ✅ duplicate check
         cur.execute("""
             SELECT COUNT(*) FROM registration
             WHERE student_id = :1 AND offering_id = :2
         """, [student_id, offering_id])
 
         if cur.fetchone()[0] > 0:
-            return {"error": "Already registered"}
+            raise HTTPException(status_code=400, detail="Already registered")
 
-        # insert
+        # ✅ credit limit check
+        cur.execute("""
+            SELECT NVL(SUM(c.credits),0)
+            FROM registration r
+            JOIN course_offering co ON r.offering_id = co.offering_id
+            JOIN course c ON co.course_id = c.course_id
+            WHERE r.student_id = :1
+        """, [student_id])
+
+        current_credits = cur.fetchone()[0]
+
+        cur.execute("""
+            SELECT c.credits
+            FROM course_offering co
+            JOIN course c ON co.course_id = c.course_id
+            WHERE co.offering_id = :1
+        """, [offering_id])
+
+        course_credits = cur.fetchone()[0]
+
+        if current_credits + course_credits > 24:
+            raise HTTPException(status_code=400, detail="Credit limit exceeded")
+
+        # ✅ insert
         cur.execute("""
             INSERT INTO registration(registration_id, student_id, offering_id)
             VALUES(reg_seq.NEXTVAL, :1, :2)
         """, [student_id, offering_id])
-    
+
         conn.commit()
 
-        return {"msg": "Course added"}
+        return {"msg": "Course registered"}
+
+    except:
+        conn.rollback()
+        raise
+
     finally:
         cur.close()
         conn.close()
