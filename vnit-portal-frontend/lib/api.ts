d11d424e82ab5aca523771ea3c76eeb4522d152c
Deepanshu Mehandia <deepanshu2109mehandia@gmail.com>
2026-03-19 16:46:31 +0530

    Update api.ts

--- vnit-portal-frontend/lib/api.ts
+++ vnit-portal-frontend/lib/api.ts
@@ -1,10 +1,12 @@
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL
+
 export async function api(url: string, options: any = {}) {
 
   const token = typeof window !== "undefined"
     ? localStorage.getItem("token")
     : null
 
-  const res = await fetch(`http://127.0.0.1:8000${url}`, {
+  const res = await fetch(`${BASE_URL}${url}`, {
     headers: {
       "Content-Type": "application/json",
       ...(token && { Authorization: `Bearer ${token}` })
@@ -14,6 +16,11 @@ export async function api(url: string, options: any = {}) {
 
   if (res.status === 401) {
     window.location.href = "/login"
+    return
+  }
+
+  if (!res.ok) {
+    throw new Error("API error")
   }
 
   return res.json()
