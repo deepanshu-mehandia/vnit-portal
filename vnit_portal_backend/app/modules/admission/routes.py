2a19a9a6af44ce680bd222de0e34ff74a7ae4627
Deepanshu Mehandia <deepanshu2109mehandia@gmail.com>
2026-04-09 13:54:31 +0000

    fixed email.py

--- vnit_portal_backend/app/modules/admission/routes.py
+++ vnit_portal_backend/app/modules/admission/routes.py
@@ -15,21 +15,51 @@ def submit_admission(data: dict):
     conn = get_connection()
     cur = conn.cursor()
 
-    # 🔹 Generate credentials
     username = data["email"]
-    raw_password = generate_password()
-    hashed_password = hash_password(raw_password)
 
-    # 🔹 1. Insert into users table
+    # 🔍 Check if user exists
     cur.execute("""
-        INSERT INTO users (username, password, role)
-        VALUES (%s, %s, %s)
-        RETURNING user_id
-    """, (username, hashed_password, "student"))
+        SELECT user_id FROM users WHERE username = %s
+    """, (username,))
+    existing_user = cur.fetchone()
 
-    user_id = cur.fetchone()[0]
+    if existing_user:
+        user_id = existing_user[0]
+        print("User already exists")
+    else:
+        raw_password = generate_password()
+        hashed_password = hash_password(raw_password)
 
-    # 🔹 2. Insert into students table (link with user_id)
+        cur.execute("""
+            INSERT INTO users (username, password, role)
+            VALUES (%s, %s, %s)
+            RETURNING user_id
+        """, (username, hashed_password, "student"))
+
+        user_id = cur.fetchone()[0]
+
+        # send email only for NEW user
+        email_sent = send_credentials_email(
+            to_email=data["email"],
+            username=username,
+            password=raw_password
+        )
+
+        if not email_sent:
+            print("Email failed but admission stored")
+
+    # 🔍 Check if student already exists
+    cur.execute("""
+        SELECT student_id FROM students WHERE user_id = %s
+    """, (user_id,))
+    existing_student = cur.fetchone()
+
+    if existing_student:
+        cur.close()
+        conn.close()
+        return {"message": "You have already applied"}
+
+    # 🔹 Insert student
     cur.execute("""
         INSERT INTO students 
         (user_id, name, email, mobile, dob, gender, category, state, address,
@@ -54,17 +84,7 @@ def submit_admission(data: dict):
     cur.close()
     conn.close()
 
-    email_sent=send_credentials_email(
-        to_email=data["email"],
-        username=username,
-        password=raw_password
-    )
-
-    if not email_sent:
-        print("Email failed but admission stored")
-    
     return {
         "message": "Admission successful",
-        "username": username,
-        "password": raw_password  # TEMP (for testing)
+        "username": username
     }
