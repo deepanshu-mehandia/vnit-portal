e5753ac162263c55c375f6aaaaa98416ccbf1fc0
Deepanshu Mehandia <deepanshu2109mehandia@gmail.com>
2026-04-10 11:14:04 +0000

    auth

--- vnit_portal_backend/app/core/dependencies.py
+++ vnit_portal_backend/app/core/dependencies.py
@@ -1,13 +1,17 @@
 from fastapi import Depends, HTTPException
-from fastapi.security import HTTPBearer
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from jose import jwt
+import os
 
 security = HTTPBearer()
-SECRET = "your-secret"
 
-def get_current_user(token=Depends(security)):
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    token = credentials.credentials
+
     try:
-        payload = jwt.decode(token.credentials, SECRET, algorithms=["HS256"])
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
         return payload
     except:
         raise HTTPException(status_code=401, detail="Invalid token")
