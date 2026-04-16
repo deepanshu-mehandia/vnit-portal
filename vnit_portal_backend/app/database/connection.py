f58acbec5418b7109524cad319bd56f34741f660
Deepanshu Mehandia <deepanshu2109mehandia@gmail.com>
2026-04-16 12:26:50 +0000

    login

--- vnit_portal_backend/app/database/connection.py
+++ vnit_portal_backend/app/database/connection.py
@@ -1,18 +1,26 @@
 import os
 from psycopg2 import pool
 
-connection_pool = pool.SimpleConnectionPool(
-    1,
-    10,
-    host=os.getenv("DB_HOST"),
-    database=os.getenv("DB_NAME"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    port=5432
-)
+connection_pool = None
+
+def init_connection_pool():
+    global connection_pool
+
+    if connection_pool is None:
+        connection_pool = pool.SimpleConnectionPool(
+            1,
+            5,  # keep LOW for Supabase free tier
+            host=os.getenv("DB_HOST"),
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            port=5432
+        )
 
 def get_connection():
+    init_connection_pool()
     return connection_pool.getconn()
 
 def release_connection(conn):
-    connection_pool.putconn(conn)
+    if connection_pool:
+        connection_pool.putconn(conn)
